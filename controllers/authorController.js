332ac791c59c46e610d2a013551f53a13162bb7f
Carlos Diaz <cdiaz86@gmail.com>
2020-10-31 20:54:34 -0400

    Removes comment.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -6,7 +6,7 @@ exports.author_list = function(req, res) {
     .sort([['family_name', 'ascending']])
     .exec(function (err, list_authors) {
       if (err) return next(err);
-      //Successful, so render
+
       res.render('author_list', { title: 'Author List', author_list: list_authors });
     });
 };
